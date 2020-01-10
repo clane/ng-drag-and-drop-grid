af3eb1ea9b86c6093f59fca252627652fd7d6fdd
Chris Lane <chlane@vmware.com>
2020-01-10 15:15:33 -0800

    grid struture added

--- dnd-grid-app/src/app/app.module.ts
+++ dnd-grid-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { LayoutGridComponent } from './layout-grid/layout-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    LayoutGridComponent
   ],
   imports: [
     BrowserModule
