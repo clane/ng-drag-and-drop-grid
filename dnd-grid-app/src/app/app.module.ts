2f39062dbb58ac759e0f06c2c3c16b9646bccf98
Chris Lane <chlane@vmware.com>
2020-01-12 18:13:35 -0800

    fixed error

--- dnd-grid-app/src/app/app.module.ts
+++ dnd-grid-app/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
 import { LayoutGridComponent } from './layout-grid/layout-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent,
     LayoutGridComponent
   ],
   imports: [
