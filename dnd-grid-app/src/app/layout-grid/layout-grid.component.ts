79a1220d6e88ee6fd1e76172b0769d910ca478a8
Chris Lane <chlane@vmware.com>
2020-01-17 11:33:16 -0800

    starting to use status region

--- dnd-grid-app/src/app/layout-grid/layout-grid.component.ts
+++ dnd-grid-app/src/app/layout-grid/layout-grid.component.ts
@@ -7,26 +7,29 @@ import { Component, ViewChild, ElementRef, Renderer2, OnInit } from '@angular/co
 })
 export class LayoutGridComponent implements OnInit {
 
-  constructor(private renderer: Renderer2 ) {}
+  constructor(private renderer: Renderer2) { }
 
-  @ViewChild('cell1', {static:true }) cell1:ElementRef;
-  @ViewChild('cell2', {static:true }) cell2:ElementRef;
-  @ViewChild('cell3', {static:true }) cell3:ElementRef;
-  @ViewChild('cell4', {static:true }) cell4:ElementRef;
-  @ViewChild('cell5', {static:true }) cell5:ElementRef;
-  @ViewChild('cell6', {static:true }) cell6:ElementRef;
-  @ViewChild('cell7', {static:true }) cell7:ElementRef;
-  @ViewChild('cell8', {static:true }) cell8:ElementRef;
-  @ViewChild('dragObj', {static:true }) dragObj:ElementRef;
+  @ViewChild('cell1', { static: true }) cell1: ElementRef;
+  @ViewChild('cell2', { static: true }) cell2: ElementRef;
+  @ViewChild('cell3', { static: true }) cell3: ElementRef;
+  @ViewChild('cell4', { static: true }) cell4: ElementRef;
+  @ViewChild('cell5', { static: true }) cell5: ElementRef;
+  @ViewChild('cell6', { static: true }) cell6: ElementRef;
+  @ViewChild('cell7', { static: true }) cell7: ElementRef;
+  @ViewChild('cell8', { static: true }) cell8: ElementRef;
+  @ViewChild('dragObj', { static: true }) dragObj: ElementRef;
+  @ViewChild('statusRegion', { static: true }) statusRegion: ElementRef;
 
-  dragObjLabel:string;
+   
 
- 
+  dragObjLabel: string;
+
+
 
   ngOnInit() {
 
-    this.dragObjLabel = "Hi I'm the drag object"; 
-    
+    this.dragObjLabel = "Hi I'm the drag object";
+
   }
 
 
@@ -43,123 +46,136 @@ export class LayoutGridComponent implements OnInit {
     var data = $event.dataTransfer.getData("text");
     $event.target.appendChild(document.getElementById(data));
   }
-  
 
-  
 
-  handleKeydownOnGridcells($event, cellNumber: number){
-    if($event.keyCode === 39){//right arrow
-      if(cellNumber === 1){
+
+
+  handleKeydownOnGridcells($event, cellNumber: number) {
+    if ($event.keyCode === 39) {//right arrow
+      if (cellNumber === 1) {
         this.cell2.nativeElement.focus();
-        
-      }
-      if(cellNumber === 2){
-        this.cell3.nativeElement.focus();
-  
-      }
-      if(cellNumber === 3){
-        this.cell4.nativeElement.focus();
        
+
       }
-      if(cellNumber === 4){
+      if (cellNumber === 2) {
+        this.cell3.nativeElement.focus();
+       
+
+      }
+      if (cellNumber === 3) {
+        this.cell4.nativeElement.focus();
+
+      }
+      if (cellNumber === 4) {
         this.cell5.nativeElement.focus();
-        
+
       }
 
-      if(cellNumber === 5){
+      if (cellNumber === 5) {
         this.cell6.nativeElement.focus();
-        
+
       }
-      if(cellNumber === 6){
+      if (cellNumber === 6) {
         this.cell7.nativeElement.focus();
-       
+
       }
-      if(cellNumber === 7){
+      if (cellNumber === 7) {
         this.cell8.nativeElement.focus();
-        
+
       }
-      if(cellNumber === 8){
+      if (cellNumber === 8) {
         this.cell1.nativeElement.focus();//cycling behavior
-     
+
       }
-     
+
     }
 
-    if($event.keyCode === 37 ){//left arrow
-      if(cellNumber === 1){
+    if ($event.keyCode === 37) {//left arrow
+      if (cellNumber === 1) {
         this.cell8.nativeElement.focus();//cycling behavior
-       
+
       }
-      if(cellNumber === 2){
+      if (cellNumber === 2) {
         this.cell1.nativeElement.focus();
-      
+
       }
-      if(cellNumber === 3){
+      if (cellNumber === 3) {
         this.cell2.nativeElement.focus();
-       
+
       }
-      if(cellNumber === 4){
+      if (cellNumber === 4) {
         this.cell3.nativeElement.focus();
-   
+
       }
 
-      if(cellNumber === 5){
+      if (cellNumber === 5) {
         this.cell4.nativeElement.focus();//cycling behavior
-        
+
       }
-      if(cellNumber === 6){
+      if (cellNumber === 6) {
         this.cell5.nativeElement.focus();
-     
+
       }
-      if(cellNumber === 7){
+      if (cellNumber === 7) {
         this.cell6.nativeElement.focus();
-      
+
       }
-      if(cellNumber === 8){
+      if (cellNumber === 8) {
         this.cell7.nativeElement.focus();
-    
+
       }
-      
-    } 
+
+    }
 
   }
 
-  handleKeydownOnDragObject() { 
+  handleKeydownOnDragObject() {
     this.cell1.nativeElement.focus();
   }
 
 
   moveContentWithFocus(cellNumber: number) {
 
-    
-    if(cellNumber === 1){
+
+    if (cellNumber === 1) {
       this.renderer.appendChild(this.cell1.nativeElement, this.dragObj.nativeElement);
     }
-    
-    if(cellNumber === 2){
+
+    if (cellNumber === 2) {
       this.renderer.appendChild(this.cell2.nativeElement, this.dragObj.nativeElement);
     }
-    if(cellNumber === 3){
+    if (cellNumber === 3) {
       this.renderer.appendChild(this.cell3.nativeElement, this.dragObj.nativeElement);
     }
-    if(cellNumber === 4){
+    if (cellNumber === 4) {
       this.renderer.appendChild(this.cell4.nativeElement, this.dragObj.nativeElement);
     }
 
-    if(cellNumber === 5){
+    if (cellNumber === 5) {
       this.renderer.appendChild(this.cell5.nativeElement, this.dragObj.nativeElement);
     }
-    
-    if(cellNumber === 6){
+
+    if (cellNumber === 6) {
       this.renderer.appendChild(this.cell6.nativeElement, this.dragObj.nativeElement);
     }
-    if(cellNumber === 7){
+    if (cellNumber === 7) {
       this.renderer.appendChild(this.cell7.nativeElement, this.dragObj.nativeElement);
     }
-    if(cellNumber === 8){
+    if (cellNumber === 8) {
       this.renderer.appendChild(this.cell8.nativeElement, this.dragObj.nativeElement);
     }
 
+    this.updateDropStatus(cellNumber);
+
   }
+
+  updateDropStatus(cellNumber: number){
+    console.log('test');
+    console.log(cellNumber);
+
+  }
+
+
+
 
 }
